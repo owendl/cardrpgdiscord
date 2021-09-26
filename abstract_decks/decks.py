35db9214c539768b29ee2c69ba0d6f0e6e1d3684
Drew Owen <owen.drew.l@gmail.com>
2021-09-26 15:44:09 -0400

    added abstract_decks and abstract deck with draw method

---
+++ abstract_decks/decks.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+import os
+
+ 
+class Deck(ABC):
+    @abstractmethod
+    def draw(self, kwargs):
+        pass
+
+class Juggernaut(Deck):
+    def __init__(self):
+        self.game = "juggernaut"
+        self.deck_folder = "decks/juggernaut"
+        self.deck_index = 0 
+
+    # def _draw(self):
+    #     print(os.listdir(self.deck_folder)[0])
+
+
+
+implemented_decks =  {"juggernaut": Juggernaut}
+
+def _implemented_games():
+    return list(implemented_decks.keys())
+
+def _get_deck(game):
+    return implemented_decks.get(game)
+
