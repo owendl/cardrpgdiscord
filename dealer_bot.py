7eb931562e6b328a81d948d828b201a2be370afc
Drew Owen <owen.drew.l@gmail.com>
2021-10-21 20:17:20 -0400

    draw command sends a series of image files. output of game.draw() command is a list of filepaths to cards to be sent to deck

--- dealer_bot.py
+++ dealer_bot.py
@@ -49,13 +49,16 @@ async def library(ctx):
     await ctx.send(" ,".join(decks._implemented_games()))
 
 @bot.command()
-async def draw(ctx):
-    await ctx.send("did a draw")
+async def draw(ctx, *args):
+    my_files = bot.games[ctx.guild.id]["game"].draw()
+
+    my_files = [discord.File(x) for x in my_files]
+    await ctx.send(files=my_files)
 
 @bot.command()
 async def play(ctx, game: str):
     if game in bot.implemented_decks:
-        bot.games[ctx.guild.id] = {"deck":bot.implemented_decks[game]()}
+        bot.games[ctx.guild.id] = {"game":bot.implemented_decks[game]()}
         bot.games[ctx.guild.id]["started"] = datetime.now()
         bot.games[ctx.guild.id]["last_action"] = datetime.now()
         await ctx.send(f"Game of {game} started on this server")
