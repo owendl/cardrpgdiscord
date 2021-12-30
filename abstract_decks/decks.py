77574a50a6dfd166f233b751164a2294094bca0c
Drew Owen <owen.drew.l@gmail.com>
2021-12-30 12:11:33 -0500

    Added ctx to abstrac method

--- abstract_decks/decks.py
+++ abstract_decks/decks.py
@@ -1,21 +1,38 @@
 from abc import ABC, abstractmethod
 import os
-
+import discord
+import glob
+import random
+import time
  
 class Deck(ABC):
     @abstractmethod
-    def draw(self, kwargs):
+    def draw(self, ctx, cmd_args):
+        pass
+    
+    @abstractmethod
+    def instructions(self, ctx, cmd_args):
         pass
 
 class Juggernaut(Deck):
     def __init__(self):
         self.game = "juggernaut"
         self.deck_folder = "decks/juggernaut/deck"
-        self.deck_index = -1 
+        self.deck_index = 0
 
-    def draw(self):
+    async def instructions(self, ctx, cmd_args):
+        instruction_text = f'''
+        Need to add        
+        '''
+        return
+
+    async def draw(self, ctx, cmd_args):
         self.deck_index += 1
-        return [os.path.join(self.deck_folder, os.listdir(self.deck_folder)[self.deck_index])]
+        await ctx.send("doing a draw")
+        time.sleep(15)
+        matches = glob.glob(f"{self.deck_folder}/*_{self.deck_index:02}.*")
+        m = random.sample(matches,1)[0]
+        return [m]
 
 
 
